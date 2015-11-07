eef679b9d03e996d481c0d3d2581ed6601a6951a
David Osztertag <devel@oszi.one>
2015-11-07 05:17:36 +0100

    Add Jekyll

---
+++ jekyll/usr/bin/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# If no image tag is specified, expose it.
+[[ $(echo ${IMAGE} | awk -F/ '{print $(NF)}') =~ ':' ]] || IMAGE="${IMAGE}:\${TAG}"
+
+cp -av /usr/share/install/jekyll* ${HOST}/usr/local/bin/
+sed -i "s;__IMAGE__;${IMAGE};g"   ${HOST}/usr/local/bin/jekyll*
---
+++ jekyll/usr/bin/uninstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -f ${HOST}/usr/local/bin/jekyll*
