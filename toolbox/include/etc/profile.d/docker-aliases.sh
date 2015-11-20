64875366fc826bd4b42caf285cc4f5e4349d5670
David Osztertag <devel@oszi.one>
2015-11-20 21:21:09 +0100

    Add dxargs and dlogs aliases

--- toolbox/include/etc/profile.d/docker-aliases.sh
+++ toolbox/include/etc/profile.d/docker-aliases.sh
@@ -4,11 +4,13 @@ alias dexec='docker exec -it'
 alias dimg='docker images'
 alias dps='docker ps -a --format="table {{.Names}}\t{{.Image}}\t{{.Command}}\t{{.Status}}"'
 alias dinspect='docker inspect'
-alias dinspect-ip='docker inspect -f "{{.NetworkSettings.IPAddress}}"'
+alias dinspect-ip='docker inspect -f {{.NetworkSettings.IPAddress}}'
+alias dxargs='xargs -L1 -P1 -r docker'
 
 dclean() {
-  docker ps -aqf 'status=dead' -f 'status=exited' -f 'status=created' | xargs -r docker rm -fv
+  docker ps -aqf 'status=dead' -f 'status=exited' -f 'status=created' | xargs -r docker rm -f
   docker images -qf 'dangling=true' | xargs -r docker rmi
+  docker volume ls -qf 'dangling=true' | xargs -r docker volume rm
 }
 
 dimggrep() {
@@ -25,4 +27,14 @@ dstats() {
 
 dupdate() {
   dimggrep "$@" | xargs -L1 -P3 -r docker pull
+}
+
+dlogs() {
+  case `docker inspect -f {{.HostConfig.LogConfig.Type}} "${!#}" 2> /dev/null \
+    || docker info | grep -m1 'Logging Driver' | awk '{print $(NF)}'` in
+  journald)
+    journalctl "${@:1:$#-1}" CONTAINER_NAME="${!#}";;
+  *)
+    docker logs "$@";;
+  esac
 }
