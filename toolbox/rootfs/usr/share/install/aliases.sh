8ae0c234ab64cbaccff7dc96a9ee872f5d7267d1
David Osztertag <devel@oszi.one>
2015-11-27 21:05:40 +0100

    Tolerate legacy Bourne Shell
    Also changed toolbox aliases.

--- toolbox/rootfs/usr/share/install/aliases.sh
+++ toolbox/rootfs/usr/share/install/aliases.sh
@@ -1,20 +1,18 @@
 toolbox() {
-  IMAGE="__IMAGE__"
-  docker run --rm -it -e TERM -e IMAGE="$IMAGE" "$@" "$IMAGE"
+  docker run -it -e TERM --rm "__IMAGE__" "$@"
 }
 
 ctoolbox() {
-  toolbox --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --security-opt=label:disable \
-  --pid=host --net="container:${!#}" --ipc="container:${!#}" \
-  --volumes-from="${!#}" "${@:1:$#-1}"
+  docker run -it -e TERM --rm --privileged --volumes-from="$1" \
+  --net="container:$1" --ipc="container:$1" --pid=host \
+  "__IMAGE__" "${@:2:$#}"
 }
 
 alias toolbox-attach='ctoolbox'
 
 htoolbox() {
   IMAGE="__IMAGE__"
-  OPTS=("$@" -e TERM)
-  eval $(docker inspect -f "{{.Config.Labels.RUN}}" "$IMAGE")
+  eval $(docker inspect -f {{.Config.Labels.RUN}} ${IMAGE}) '"$@"'
 }
 
 alias toolbox-host='htoolbox'
