3a454d6e4d807b3f5ea036119c6833f33cf95b0b
David Osztertag <devel@oszi.one>
2017-02-26 00:11:53 +0100

    Remove Jekyll

--- jekyll/artifacts/atomic-uninstall.sh
+++
@@ -1,3 +0,0 @@
-if [ $(find_instances | wc -l) -eq 0 ]; then
-  rm -fv ${HOST}/etc/profile.d/jekyll_container.sh
-fi
--- jekyll/artifacts/jekyll.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eu
-SUDO_UID=$(id -u)
-SUDO_GID=$(id -g)
-. /etc/sysconfig/containers/jekyll
-eval exec $(docker inspect -f {{.Config.Labels.RUN}} ${IMAGE}) '"$@"'
