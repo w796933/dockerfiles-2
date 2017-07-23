1b9b66750baff880a5e806be4bc8621d01a42dea
David Osztertag <root@oszi.one>
2017-07-23 11:32:15 +0200

    Update OpenVPN atomic install

--- openvpn/artifacts/atomic-install.sh
+++ openvpn/artifacts/atomic-install.sh
@@ -8,11 +8,11 @@ cd ${HOST}/${CONFDIR}
 
 if [ ! -d ${EASYRSA_PKI} ]; then
   easyrsa init-pki
+# easyrsa build-ca && easyrsa gen-crl && easyrsa gen-dh
+# easyrsa build-server-full $(hostname)
+  openvpn --genkey --secret ${EASYRSA_PKI}/ta.key
   cp -v ${EASYRSA}/vars.example ${EASYRSA_PKI}/vars
   touch -a ${NAME}.conf
 fi
 
 /artifacts/atomic-update.sh
-
-echo 'Dropping into an interactive shell...'
-bash -i || :
