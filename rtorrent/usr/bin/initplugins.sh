9caf68388197b6060593d618237fd19603e87360
David Osztertag <root@oszi.one>
2017-08-25 19:30:46 +0200

    Fix scripts in rtorrent

--- rtorrent/usr/bin/initplugins.sh
+++ rtorrent/usr/bin/initplugins.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -u
 awk -F: '{print $1}' /data/.htpasswd \
-| xargs -n1 /usr/bin/php /var/www/rutorrent/php/initplugins.php &
+| xargs -n1 /usr/bin/php /usr/share/rutorrent/php/initplugins.php &
